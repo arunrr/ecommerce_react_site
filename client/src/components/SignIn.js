9f6ca4ad3aa063ca065f647ee79cd2585145de8d
Arun R R <sparkingkings@gmail.com>
2020-01-15 15:03:58 +0530

    Implemented simple components with routing

---
+++ client/src/components/SignIn.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class LogOut extends Component {
+  render() {
+    return <div>SignIn</div>;
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './components/App';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import LogOut from './components/LogOut';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={App} exact path="/" />
+        <Route component={SignUp} path="/signup" />
+        <Route component={SignIn} path="/signin" />
+        <Route component={LogOut} path="/logout" />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
