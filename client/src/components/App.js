ffb7a23f1bc93b9e723c071b585fe97dbd3dd0ba
Arun R R <sparkingkings@gmail.com>
2020-01-17 20:38:00 +0530

    Add strapi server api

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container, Box, Heading } from 'gestalt';
+import Strapi from 'strapi-sdk-javascript/build/main';
+
 import './App.css';
 
-function App() {
-  return <div>App</div>;
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
+
+class App extends Component {
+  async componentDidMount() {
+    const response = await strapi.request('POST', '/graphql', {
+      data: {
+        query: ` query{
+            brands{
+              _id
+              name
+              description
+              createdAt
+              image{
+                name
+              }
+            }
+          }
+          
+          
+          `
+      }
+    });
+
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <Container>
+        {/* Brands Section */}
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          {/* Brands Header */}
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
+  }
 }
 
 export default App;
