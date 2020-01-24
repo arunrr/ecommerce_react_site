d62dcc3bdb3b4ecb7e4c10ab63e2194b43e7b774
Arun R R <sparkingkings@gmail.com>
2020-01-24 13:56:36 +0530

    Added conditional based on cart items

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Container, Box, Heading, TextField, Text } from 'gestalt';
 
 import { getCart, displayTotalPrice } from '../utils';
@@ -82,90 +82,100 @@ class Checkout extends Component {
           </Box>
 
           {/* Cart items section */}
-
-          <Box
-            display="flex"
-            marginBottom={4}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Box margin={2}>
-              <Text italics color="orchid" size="md">
-                {cartItems.length} items to checkout
-              </Text>
-            </Box>
-            <Box>
-              {cartItems.map(item => (
-                <Box key={item.id} margin={4}>
-                  <Text>
-                    {` ${item.name} x ${item.quantity} --> 
-                    ${item.price * item.quantity} $`}
+          {cartItems.length !== 0 ? (
+            <Fragment>
+              <Box
+                display="flex"
+                marginBottom={4}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Box margin={2}>
+                  <Text italics color="orchid" size="md">
+                    {cartItems.length} items to checkout
                   </Text>
                 </Box>
-              ))}
+                <Box>
+                  {cartItems.map(item => (
+                    <Box key={item.id} margin={4}>
+                      <Text>
+                        {` ${item.name} x ${item.quantity} --> 
+                    ${item.price * item.quantity} $`}
+                      </Text>
+                    </Box>
+                  ))}
+                </Box>
+                <Text size="lg" color="watermelon">
+                  Total : {displayTotalPrice(cartItems)}
+                </Text>
+              </Box>
+              {/* Checkout form */}
+              <form
+                style={{
+                  display: 'inlineBlock',
+                  maxWidth: 450,
+                  textAlign: 'center'
+                }}
+                onSubmit={this.handleSubmit}
+              >
+                {/* Address Input */}
+                <Box marginBottom={3} width={500}>
+                  <TextField
+                    id="address"
+                    name="address"
+                    type="text"
+                    placeholder="Enter Shipping Address"
+                    onChange={this.handleChange}
+                  />
+                </Box>
+                {/* Postal Code Input */}
+                <Box marginBottom={3} width={500}>
+                  <TextField
+                    id="postalCode"
+                    name="postalCode"
+                    type="number"
+                    placeholder="Enter Postal Code"
+                    onChange={this.handleChange}
+                  />
+                </Box>
+                {/* City Input */}
+                <Box marginBottom={3} width={500}>
+                  <TextField
+                    id="city"
+                    name="city"
+                    type="text"
+                    placeholder="City of Residence"
+                    onChange={this.handleChange}
+                  />
+                </Box>
+                {/* Confirm Email Input */}
+                <Box marginBottom={3} width={500}>
+                  <TextField
+                    id="confirmEmail"
+                    name="confirmEmail"
+                    type="email"
+                    placeholder="Enter registered email address"
+                    onChange={this.handleChange}
+                  />
+                </Box>
+                {/* Checkout button */}
+                <Box>
+                  <button id="stripe__button" type="submit">
+                    Checkout
+                  </button>
+                </Box>
+              </form>
+            </Fragment>
+          ) : (
+            <Box margin={2}>
+              <Text color="watermelon" size="lg">
+                Please add items to cart
+              </Text>
             </Box>
-            <Text size="lg" color="watermelon">
-              Total : {displayTotalPrice(cartItems)}
-            </Text>
-          </Box>
-          {/* Checkout form */}
-          <form
-            style={{
-              display: 'inlineBlock',
-              maxWidth: 450,
-              textAlign: 'center'
-            }}
-            onSubmit={this.handleSubmit}
-          >
-            {/* Address Input */}
-            <Box marginBottom={3} width={500}>
-              <TextField
-                id="address"
-                name="address"
-                type="text"
-                placeholder="Enter Shipping Address"
-                onChange={this.handleChange}
-              />
-            </Box>
-            {/* Postal Code Input */}
-            <Box marginBottom={3} width={500}>
-              <TextField
-                id="postalCode"
-                name="postalCode"
-                type="number"
-                placeholder="Enter Postal Code"
-                onChange={this.handleChange}
-              />
-            </Box>
-            {/* City Input */}
-            <Box marginBottom={3} width={500}>
-              <TextField
-                id="city"
-                name="city"
-                type="text"
-                placeholder="City of Residence"
-                onChange={this.handleChange}
-              />
-            </Box>
-            {/* Confirm Email Input */}
-            <Box marginBottom={3} width={500}>
-              <TextField
-                id="confirmEmail"
-                name="confirmEmail"
-                type="email"
-                placeholder="Enter registered email address"
-                onChange={this.handleChange}
-              />
-            </Box>
-            {/* Checkout button */}
-            <Box>
-              <button id="stripe__button" type="submit">
-                Checkout
-              </button>
-            </Box>
-          </form>
+          )}
         </Box>
+
         {/* Toast message section */}
         <Box
           position="fixed"
