9359448d924136bdda8f1204839c2b8e0fdf5ef7
Arun R R <sparkingkings@gmail.com>
2020-02-05 00:24:58 +0530

    Updates

--- precache-manifest.6f8260fe343898a3f421d4376fbe04a2.js
+++ precache-manifest.6f8260fe343898a3f421d4376fbe04a2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfd3879bdc2ae22ac3904626be29555b",
+    "revision": "78a614397d45e60bec9f7005ad488eeb",
     "url": "/ecommerce_react_site/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ecommerce_react_site/static/css/2.13cf2586.chunk.css"
   },
   {
-    "revision": "191758245c351920145d",
+    "revision": "cfc4ba06237096a79fdb",
     "url": "/ecommerce_react_site/static/css/main.1641692a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ecommerce_react_site/static/js/2.3c368036.chunk.js.LICENSE"
   },
   {
-    "revision": "191758245c351920145d",
-    "url": "/ecommerce_react_site/static/js/main.56c6b1e9.chunk.js"
+    "revision": "cfc4ba06237096a79fdb",
+    "url": "/ecommerce_react_site/static/js/main.95fa6951.chunk.js"
   },
   {
     "revision": "04669af3f17260c04cf8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecommerce_react_site/precache-manifest.2fb92a23d4ee966d52407c5ea779d2bc.js"
+  "/ecommerce_react_site/precache-manifest.6f8260fe343898a3f421d4376fbe04a2.js"
 );
 
 self.addEventListener('message', (event) => {
